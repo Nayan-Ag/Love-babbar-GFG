33fc2c4c556caf4b7877ddd668645d5939e2bc6c
Nayan Agarwal <108821316+Nayan-Ag@users.noreply.github.com>
2023-06-26 17:01:23 +0530

    Update and rename Reverse a linked list to Reverse a linked list.java

--- LinkedList/Reverse a linked list.java
+++ LinkedList/Reverse a linked list.java
@@ -1,11 +1,13 @@
-Ques: Given a linked list of N nodes. The task is to reverse this list.
-Input:
-LinkedList: 1->2->3->4->5->6
-Output: 6 5 4 3 2 1
-Explanation: After reversing the list, 
-elements are 6->5->4->3->2->1.
+// Ques: Given a linked list of N nodes. The task is to reverse this list.
 
-Code:
+// Input:
+// LinkedList: 1->2->3->4->5->6
+// Output: 6 5 4 3 2 1
+
+// Explanation: After reversing the list, 
+// elements are 6->5->4->3->2->1.
+
+// Code:
 class Solution
 {
     //Function to reverse a linked list.

5c48b0e44348a95b8476df1636b03d98b47744b3
Nayan Agarwal <108821316+Nayan-Ag@users.noreply.github.com>
2023-06-26 16:59:57 +0530

    Update and rename Remove duplicates from an unsorted linked list to Remove duplicates from an unsorted linked list.java

--- LinkedList/Remove duplicates from an unsorted linked list.java
+++ LinkedList/Remove duplicates from an unsorted linked list.java
@@ -1,18 +1,20 @@
-Ques: Given an unsorted linked list of N nodes. The task is to remove duplicate elements from this unsorted Linked List. When a value appears in multiple nodes, the node which appeared first should be kept, all others duplicates are to be removed.
-Input:
-N = 4
-value[] = {5,2,2,4}
-Output: 5 2 4
-Explanation:Given linked list elements are
-5->2->2->4, in which 2 is repeated only.
-So, we will delete the extra repeated
-elements 2 from the linked list and the
-resultant linked list will contain 5->2->4
+// Ques: Given an unsorted linked list of N nodes. The task is to remove duplicate elements from this unsorted Linked List. When a value appears in multiple nodes, the node which appeared first should be kept, all others duplicates are to be removed.
 
-Expected Time Complexity: O(N)
-Expected Auxiliary Space: O(N)
+// Input:
+// N = 4
+// value[] = {5,2,2,4}
+// Output: 5 2 4
 
-Code:
+// Explanation:Given linked list elements are
+// 5->2->2->4, in which 2 is repeated only.
+// So, we will delete the extra repeated
+// elements 2 from the linked list and the
+// resultant linked list will contain 5->2->4
+
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(N)
+
+// Code:
 class Solution
 {
     //Function to remove duplicates from unsorted linked list.
